1914318a08deac0a8ac5d9ed924d330ee3f35863
Avanthi Pathiraja <avanthilpathiraja@gmail.com>
2021-10-04 11:29:25 +0530

    Issue Note table search state 1 done

--- app/Http/Livewire/IssueNote/Index.php
+++ app/Http/Livewire/IssueNote/Index.php
@@ -23,7 +23,19 @@ class Index extends IssueNoteCreate
     public function render()
     {
         $issue_notes = IssueNote::where('is_active',1)
-            ->where(DB::raw('concat(number,date)'),'like','%'.$this->search.'%')
+            ->where(function($issue_note){
+                $issue_note
+                    ->where('number','like','%'.$this->search.'%')
+                    ->orWhere('date','like','%'.$this->search.'%')
+                    ->orWhere('reference','like','%'.$this->search.'%');
+            })
+            ->orWhere(function($distributor){
+                $distributor
+                ->whereHas('distributor',function($distributor1){
+                    $distributor1
+                        ->where(DB::raw('concat(title," ",first_name," ",last_name)'),'like','%'.$this->search.'%');
+                });
+            })
             ->paginate(10);
 
         return view('livewire.issue-note.index')
