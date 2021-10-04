2a2e6ebdbc0ac138ae350431c2469d4da1233dbb
Avanthi Pathiraja <avanthilpathiraja@gmail.com>
2021-10-04 14:19:38 +0530

    Invoice payment table search stage 1 done

--- app/Http/Livewire/InvoicePayment/Index.php
+++ app/Http/Livewire/InvoicePayment/Index.php
@@ -23,7 +23,19 @@ class Index extends InvoicePaymentCreate
     public function render()
     {
         $invoice_payments = InvoicePayment::where('is_active',1)
-            ->where(DB::raw('concat(number,reference,date)'),'like','%'.$this->search.'%')
+            ->where(function($invoice_payment){
+                $invoice_payment
+                ->where('number','like','%'.$this->search.'%')
+                ->orWhere('reference','like','%'.$this->search.'%')
+                ->orWhere('date','like','%'.$this->search.'%')
+                ->orWhere('note','like','%'.$this->search.'%')
+                ->orWhere('amount','like','%'.$this->search.'%')
+                ->orWhere('payment_type','like','%'.$this->search.'%');
+            })
+            ->orWhereHas('customer',function($customer){
+                $customer
+                ->where('name','like','%'.$this->search.'%');
+            })
             ->paginate(10);
 
         return view('livewire.invoice-payment.index')
