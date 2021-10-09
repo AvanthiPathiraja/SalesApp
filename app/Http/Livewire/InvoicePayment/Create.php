b9747f78d2224992921eecc12cd7cfa75ab3962b
Avanthi Pathiraja <avanthilpathiraja@gmail.com>
2021-10-09 16:19:40 +0530

    invoice payment test basic 1 done

--- app/Http/Livewire/InvoicePayment/Create.php
+++ app/Http/Livewire/InvoicePayment/Create.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Livewire\InvoicePayment;
 
-use App\Models\InvoicePayment;
+use App\Models\Invoice;
 use Livewire\Component;
 use App\Models\Customer;
+use App\Models\InvoicePayment;
+use Illuminate\Support\Facades\DB;
 
 class Create extends Component
 {
     public $customers = [];
-    public $customer_id;
+    public $customer_id,$due_amount;
 
     public $invoice_payment;
     public $invoice_payment_id,$date,$number,$reference,$payment_type,$amount,$note;
@@ -18,6 +20,8 @@ class Create extends Component
     public function mount()
     {
         $this->customers = Customer::where('is_active',1)->get();
+        $this->date = date('Y-m-d');
+        $this->payment_type = 'Cash';
 
         if($this->invoice_payment)
         {
@@ -26,6 +30,7 @@ class Create extends Component
             $this->invoice_payment_id = $this->invoice_payment->id;
             $this->customer_id = $this->invoice_payment->customer_id;
             $this->date = $this->invoice_payment->date;
+            $this->number = $this->invoice_payment->number;
             $this->reference = $this->invoice_payment->reference;
             $this->payment_type = $this->invoice_payment->payment_type;
             $this->amount = $this->invoice_payment->amount;
@@ -33,21 +38,54 @@ class Create extends Component
         }
     }
 
+    public function updatedCustomerId()
+    {
+        if($this->customer_id)
+        {
+            $customer_invoices = Invoice::select(DB::raw('sum(total_price-total_discount) as payment_due'))
+                ->where('customer_id',$this->customer_id)
+                ->where('is_active',1)
+                ->first();
+
+            $customer_payments = InvoicePayment::select(DB::raw('sum(amount) as paid_total'))
+                ->where('customer_id',$this->customer_id)
+                ->where('is_active',1)
+                ->first();
+
+            $this->due_amount =  ($customer_invoices->payment_due -  $customer_payments->paid_total) ?? '0';
+        }
+    }
 
     public function saveOrUpdateInvoicePayment()
     {
         $validated_data = $this->validate([
-            'number' => 'required|numeric',
-            'reference' => 'required|max:20',
+            'number' => 'required|max:20',
+            'reference' => 'nullable|max:20',
             'date' => 'required|date',
             'customer_id' => 'required|numeric',
             'payment_type' => 'required',
-            'amount' => 'required|numeric',
+            'amount' => 'required|numeric|min:1',
             'note' => 'nullable|max:150',
         ]);
 
-        InvoicePayment::updateOrCreate($validated_data);
-        session()->flash('success','Completed Successfully !');
+        if($this->due_amount < $this->amount)
+        {
+            session()->flash('invalidAmount','Invalid amount. Please try again.');
+        }
+        else
+        {
+            InvoicePayment::updateOrCreate(['id' => $this->invoice_payment_id ?? null],$validated_data);
+            session()->flash('success','Completed Successfully !');
+            $this->resetPayment();
+        }
+    }
+
+    public function resetPayment()
+    {
+        $this->reset(['invoice_payment','invoice_payment_id','customer_id','date','number','reference','payment_type','amount','note',
+        'due_amount']);
+        $this->date = date('Y-m-d');
+        $this->payment_type = 'Cash';
     }
 
     public function deleteInvoicePayment(InvoicePayment $invoice_payment)
--- resources/views/livewire/invoice-payment/create.blade.php
+++ resources/views/livewire/invoice-payment/create.blade.php
@@ -3,13 +3,13 @@
 
         <x-table-top>
             <div class=" flex space-x-2">
-                <a href="{{ route('customer.index') }}">
+                <a href="{{ route('invoice-payment.index') }}">
                     <x-btn-back/>
                 </a>
-                <x-table-top-title>Client Info </x-table-top-title>
+                <x-table-top-title> Customer Payments Info </x-table-top-title>
             </div>
         </x-table-top>
-        <div class="grid grid-cols-6 gap-3 gap-x-4">
+        <div class="grid grid-cols-5 gap-3 gap-x-4">
 
             <x-lable class=" col-span-2">
                 <span>Customer</span>
@@ -17,12 +17,19 @@
                     <option value=""></option>
                     @foreach ( $customers as $customer )
                     <option value="{{ $customer->id }}">
-                        {{ $customer->name }}
+                        {{"{$customer->number} - {$customer->name}" }}
                     </option>
                     @endforeach
                 </x-select>
                 <x-form-error for="customer_id" />
             </x-lable>
+
+            <x-lable>
+                <span>Total Due amount</span>
+                <x-text-input wire:model="due_amount" readonly />
+            </x-lable>
+
+            <hr class=" col-span-5">
 
             <x-lable>
                 <span>Reciept Number</span>
@@ -44,7 +51,12 @@
 
             <x-lable>
                 <span>Payment Type</span>
-                <x-text-input wire:model="payment_type" />
+                    <x-select wire:model="payment_type">
+                        <option value=""></option>
+                        <option value="Cash"> Cash </option>
+                        <option value="Cheque"> Cheque </option>
+                        <option value="Other"> Other </option>
+                    </x-select>
                 <x-form-error for='payment_type' />
             </x-lable>
 
@@ -60,8 +72,9 @@
                 <x-form-error for="note" />
             </x-lable>
 
-            <x-form-footer class="col-span-6">
+            <x-form-footer class="col-span-5">
                 <x-flash-msg type="success" key="success" />
+                <x-flash-msg type="error" key="invalidAmount" />
                 <x-btn-primary wire:click.prevent='saveOrUpdateInvoicePayment()'>
                     Save
                 </x-btn-primary>
