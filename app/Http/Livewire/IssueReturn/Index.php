1586148939bb4e0b87b82792cbaf8ed59e30d5e7
Avanthi Pathiraja <avanthilpathiraja@gmail.com>
2021-10-10 15:26:30 +0530

    distributer Stock report

--- app/Http/Livewire/IssueReturn/Index.php
+++ app/Http/Livewire/IssueReturn/Index.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Livewire\IssueReturn;
 
+use App\Models\InvoiceItem;
+use App\Models\InvoiceReturn;
+use App\Models\IssueItem;
+use App\Models\IssueNote;
 use Livewire\Component;
 use App\Models\IssueReturn;
+use App\Models\Stock;
 use Livewire\WithPagination;
 use Illuminate\Support\Facades\DB;
 
@@ -13,6 +18,7 @@ class Index extends Component
 
     public $search;
     protected $listeners = ['search'];
+    public $distributor_id = 1;
 
 
     public function search($val)
@@ -20,32 +26,69 @@ class Index extends Component
         $this->search = '%'.$val.'%';
     }
 
+    public function stock_balance_data_set()
+    {
+        $issue_items = IssueItem::select('stock_id',DB::raw('sum(quantity) as issuedQuantity,'.'0 as returnedQuantity, 0 as invoicedQuantity'))
+        ->where('is_cleared',0)
+        ->whereHas('issue_note',function($issue_note){
+            $issue_note
+            ->where('distributor_id',$this->distributor_id);
+        })
+        ->groupBy('stock_id')
+        ->get()->collect();
+
+        $invoice_returns = InvoiceReturn::select('stock_id',DB::raw('0 as issuedQuantity,sum(quantity) as returnedQuantity, 0 as invoicedQuantity'))
+                ->where('is_cleared',0)
+                ->where('distributor_id',$this->distributor_id)
+                ->groupBy('stock_id')
+                ->get()->collect();
+
+        $invoice_items = InvoiceItem::select('stock_id',DB::raw('0 as issuedQuantity,0 as returnedQuantity, sum(quantity) as invoicedQuantity'))
+                ->where('is_cleared',0)
+                ->whereHas('invoice',function($invoice){
+                    $invoice->where('distributor_id',$this->distributor_id);
+                })->groupBy('stock_id')
+                ->get()->collect();
+
+
+        $stocks = $invoice_items->merge($issue_items)->merge($invoice_returns)->unique('stock_id');
+
+        $stock_products = Stock::with('product')->whereIn('id', $stocks->pluck('stock_id'))->get();
+
+        return $this->map_stock_data($stocks, $issue_items,$invoice_returns, $invoice_items, $stock_products);
+
+    }
+
+    public function map_stock_data($stocks, $issue_items,$invoice_returns, $invoice_items, $stock_products)
+    {
+        return $stocks->map(function ($stock) use($issue_items,$invoice_returns, $invoice_items, $stock_products) {
+
+            $issue_quantity = $issue_items->where('stock_id', $stock->stock_id)->first();
+            $invoice_quantity = $invoice_items->where('stock_id', $stock->stock_id)->first();
+            $return_quantity = $invoice_returns->where('stock_id', $stock->stock_id)->first();
+
+            $stock_product = $stock_products->where('id', $stock->stock_id)->first();
+
+            return [
+                'product_name' => $stock_product->product->name,
+                'issue_quantity' => $issue_quantity->issuedQuantity,
+                'invoice_quantity' => $invoice_quantity->invoicedQuantity,
+                'return_quantity' => $return_quantity->returnedQuantity
+            ];
+
+        });
+    }
+
+
+
     public function render()
     {
-        $issue_returns = IssueReturn::where(function($invoice_return){
-            $invoice_return
-            ->where('date','like','%'.$this->search.'%')
-            ->orWhere('quantity','like','%'.$this->search.'%')
-            ->orWhere('reason','like','%'.$this->search.'%');
-        })
-        ->orWhereHas('distributor',function ($distributor){
-                $distributor
-                ->where(DB::raw('concat(title," ",first_name," ",last_name)'),'like','%'.$this->search.'%');
-        })
-        ->orWhereHas('stock',function ($stock){
-                $stock
-                ->where('number','like','%'.$this->search.'%')
-                ->orWhereHas('product',function($product){
-                        $product
-                        ->where('category','like','%'.$this->search.'%')
-                        ->orWhere('name','like','%'.$this->search.'%');
-                    });
-        })
 
 
-        ->paginate(10);
+        dd($this->stock_balance_data_set());
 
-        return view('livewire.issue-return.index')
-            ->with(['issue_returns' => $issue_returns ]);
+
+        return view('livewire.issue-return.index');
+           // ->with(['issue_returns' => $issue_returns ]);
     }
 }
